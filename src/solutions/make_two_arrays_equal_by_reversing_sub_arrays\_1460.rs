d72a90bfd5c9e69e966d75f73f687617b15e09df
s1m0n21 <s1m0n21@hotmail.com>
2022-08-26 19:41:34 +0800

    cleanup

--- src/solutions/make_two_arrays_equal_by_reversing_sub_arrays\_1460.rs
+++
@@ -1,6 +0,0 @@
-// PROBLEM: https://leetcode.cn/problems/make_two_arrays_equal_by_reversing_sub_arrays
-// DATE:    2022/08/24 02:29:51 +0800
-
-// PROBLEM: https://leetcode.cn/problems/make_two_arrays_equal_by_reversing_sub_arrays
-// DATE:    2022/08/24 02:31:52 +0800
-
